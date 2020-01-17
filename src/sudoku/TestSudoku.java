db3260b877a3cbb56ca9059f3cb3b0b94fd91347
luke <luke@LAPTOP-8KU0M8RL>
2020-01-18 00:10:31 +0100

    ajout des méthodes score, niveau de difficulté et jouer une partie en accédant à la pluparts des méthodes

--- src/sudoku/TestSudoku.java
+++ src/sudoku/TestSudoku.java
@@ -19,7 +19,7 @@ public class TestSudoku {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        test_sudokuSauvegarde();
+        test_jouerUnePartie();
     }
     
     
@@ -181,10 +181,10 @@ public class TestSudoku {
         //System.out.println(cal.get(Calendar.HOUR_OF_DAY)+"h "+cal.get(Calendar.MINUTE)+"m et "+cal.get(Calendar.SECOND)+"s"); 
     }
     
-    public static void test_jouerAvecUnCandidat(){
+    public static void test_jouerAvecUnCandidat() {
         Sudoku s1 = Sudoku.chargerGrille("nouvelle_grille");
-        s1.afficheSudoku();
-        s1.jouerUnCoupAvecCandidats();
+        //s1.afficheSudoku();
+        //s1.jouerUnCoupAvecCandidats();
         //s1.getGrille().candidatsEnTropColonne(2, 1);
         //System.out.println("column "+s1.getGrille().getColumn(2));
         //System.out.println("line "+s1.getGrille().getLine(2));
@@ -195,8 +195,25 @@ public class TestSudoku {
         //s1.afficheSolution();
         //s1.help1();
         //s1.help2();
-        //s1.help3();
+        //s1.help3(1,1);
+        //s1.help4();
         //s1.enleverCandidat();
         //s1.playGameWithCandidat();
+    }
+
+    public static void test_jouerUnePartie() {
+        Sudoku s1 = Sudoku.playCommande();
+        //Joueur j = new Joueur("Nom",0);
+        //Sudoku s = new Sudoku (j,30, 3);
+        //s.afficheSudoku();
+        /*Calendar cal = Calendar.getInstance();
+        System.out.println(cal.get(Calendar.HOUR_OF_DAY)+"h "+cal.get(Calendar.MINUTE)+"m et "+cal.get(Calendar.SECOND)+"s"); 
+        Calendar ca2 = Calendar.getInstance();
+        System.out.println(ca2.get(Calendar.HOUR_OF_DAY)+"h "+ca2.get(Calendar.MINUTE)+"m et "+(ca2.get(Calendar.SECOND)+1)+"s"); 
+        int h = ca2.get(Calendar.HOUR_OF_DAY)-cal.get(Calendar.HOUR_OF_DAY);
+        int m = ca2.get(Calendar.MINUTE)-cal.get(Calendar.MINUTE);
+        int s = (ca2.get(Calendar.SECOND)+65)-cal.get(Calendar.SECOND);
+        System.out.println("h "+h+"m "+m+"s "+s);
+        System.out.println("seconde "+(h*360+m*60+s)/30);*/
     }
 }
